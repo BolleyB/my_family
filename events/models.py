75eb3549c9da577200623b484b01332dde7a462f
Hugo Webster <BolleyB>
2024-03-12 16:38:31 -0400

    init

--- events/models.py
+++ events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organized_events')
+    attendees = models.ManyToManyField(User, related_name='attending_events', through='Attendance')
+
+class Attendance(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    attendee = models.ForeignKey(User, on_delete=models.CASCADE)
+    response = models.BooleanField(default=False)
