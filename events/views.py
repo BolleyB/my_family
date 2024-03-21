906b766cd7c4495d224dbf544b47983661201f48
jademoslares <jade.moslares@gmail.com>
2024-03-21 15:19:05 -0400

    Profile rework plus list of attending events

--- events/views.py
+++ events/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
 
-
-from .models import UserForm, Event, Profile, Notification
-from .models import UserForm,Event
-from .models import UserForm, Event, Profile, Notification, Invitation
+from .models import UserForm, Event, Profile, Notification, Invitation, Attendance
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 # Add the following import for the login_required decorator
@@ -41,9 +38,14 @@ def notifications(request):
 def profile(request):
     user = request.user
     profile = Profile.objects.get(user=user)
-    events = user.attending_events.all()
+
+    usergetid = User.objects.get(username=user)
+    user_id = usergetid.id
+    print(user_id)
+    attended_events = Event.objects.filter(attendance__attendee_id=user_id)
+
     friends = profile.friends.all()
-    context = {'profile': profile, 'events': events, 'friends': friends, 'user': user}
+    context = {'profile': profile, 'events': attended_events, 'friends': friends, 'user': user}
     return render(request, 'profile.html', context)
 
 
@@ -53,6 +55,10 @@ def signup(request):
     form = UserForm(request.POST)
     if form.is_valid():
       user = form.save()
+      profile = Profile(user=user)
+      profile.save()
+      user.profile = profile
+      user.save()
       login(request, user)
       return redirect('home')
     else:
@@ -61,10 +67,10 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-def profile(request):
-    return render(request, 'profile.html')
 
-class EventCreate(CreateView):
+
+
+class EventCreate(LoginRequiredMixin,CreateView):
   model = Event
   fields = ['name', 'date', 'time', 'location', 'description', 'cost']
   success_url = '/'
@@ -79,11 +85,13 @@ def events_detail(request, event_id):
       'event': event,
    })
 
+@login_required
 def attend_event(request, event_id):
    event = get_object_or_404(Event, pk=event_id)
    event.attendees.add(request.user)
    return redirect('events_detail', event_id=event_id)
 
+@login_required
 def cancel_attend_event(request, event_id):
    event = get_object_or_404(Event, pk=event_id)
    event.attendees.remove(request.user)
@@ -103,7 +111,7 @@ def search_view(request):
     return render(request, 'search_results.html', {'query': query, 'results': results, 'query2': query2})
 
 
-
+@login_required
 def invite_friends(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
 
