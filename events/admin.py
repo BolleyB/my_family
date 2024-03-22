c53295c215d53c32be9aa722e012fce6e935adfe
Hugo Webster <BolleyB>
2024-03-21 20:07:51 -0400

    admin

---
+++ events/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Profile, Event, Notification
+# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')  # Customize the fields displayed in the admin list view
+    list_filter = ('user__username',)  # Add filters for user's username
+    search_fields = ('user__username', 'bio')  # Add search fields for user's username and bio
+
+
+admin.site.register(Event)
+admin.site.register(Notification)
